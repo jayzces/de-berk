35dd7c43dc4785fe0fff35a5dbd96da067e93ce2
Mary Louise Hermosa <jayzces@gmail.com>
2024-08-06 15:05:11 +0200

    Different method to post results

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
 
-const today = new Date()
-const nextDay = ref(new Date(today))
+const minDay = ref(new Date())
+minDay.value.setDate(minDay.value.getDate() + 3)
+const nextDay = ref(new Date(minDay.value))
 nextDay.value.setDate(nextDay.value.getDate() + 1)
 
 const checkIn = ref('')
+const successState = ref(false)
 
 const formatDate = (date: Date) => {
   return `${date.getFullYear()}-${padNumber(date.getMonth() + 1)}-${padNumber(date.getDate())}`
@@ -20,56 +22,82 @@ watch(checkIn, (newVal) => {
   nextDay.value = newCheckIn
   nextDay.value.setDate(nextDay.value.getDate() + 1)
 })
+
+const submit = (event: Event) => {
+  if (!event.target) return
+  const myForm = event.target
+  const formData = new FormData(myForm as HTMLFormElement)
+
+  if (process.env.NODE_ENV !== 'development') {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData as any).toString()
+    })
+      .then(() => (window.location.href = '/thank-you/'))
+      .catch((error) => alert(error))
+  } else {
+    successState.value = true
+  }
+}
 </script>
 
 <template>
   <form
-    class="bg-russet/70 rounded-xl px-5 py-6 w-[420px] relative flex flex-wrap gap-4"
-    netlify
-    name="contact-form"
-    method="POST"
+    class="bg-russet/70 rounded-xl px-5 py-6 w-[420px] relative"
+    @submit.prevent="submit($event)"
   >
-    <h5 class="text-cream w-full text-center">Contact us to book your room</h5>
-    <input
-      type="text"
-      name="name"
-      class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
-      placeholder="Name"
-      required
-    />
-    <input
-      type="text"
-      name="mobile"
-      class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
-      placeholder="Phone Number"
-      required
-    />
-    <input
-      type="email"
-      name="email"
-      class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
-      placeholder="Email"
-      required
-    />
-    <input
-      type="date"
-      name="check-in"
-      class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-[calc(50%_-_8px)]"
-      :min="formatDate(today)"
-      placeholder="Check In"
-      v-model="checkIn"
-      required
-    />
-    <input
-      type="date"
-      name="check-out"
-      class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-[calc(50%_-_8px)]"
-      :min="formatDate(nextDay)"
-      placeholder="Check Out"
-      required
-    />
-    <button type="submit" class="w-full rounded-md bg-olive text-cream leading-[60px]">
-      Contact
-    </button>
+    <div class="flex flex-wrap gap-4" :class="successState ? 'opacity-0' : 'opacity-100'">
+      <h5 class="text-cream w-full text-center">Contact us to book your room</h5>
+      <input
+        type="text"
+        name="name"
+        class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
+        placeholder="Name"
+        required
+      />
+      <input
+        type="text"
+        name="mobile"
+        class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
+        placeholder="Phone Number"
+        required
+      />
+      <input
+        type="email"
+        name="email"
+        class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
+        placeholder="Email"
+        required
+      />
+      <input
+        type="date"
+        name="check-in"
+        class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-[calc(50%_-_8px)]"
+        :min="formatDate(minDay)"
+        placeholder="Check In"
+        v-model="checkIn"
+        required
+      />
+      <input
+        type="date"
+        name="check-out"
+        class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-[calc(50%_-_8px)]"
+        :min="formatDate(nextDay)"
+        placeholder="Check Out"
+        required
+      />
+      <button type="submit" class="w-full rounded-md bg-olive text-cream leading-[60px]">
+        Contact
+      </button>
+    </div>
+
+    <div
+      v-if="successState"
+      class="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center"
+    >
+      <h5 class="roboto-serif text-xl text-spring-sage">Success!</h5>
+      <p class="mt-2 text-cream">For debugging only</p>
+    </div>
   </form>
 </template>
