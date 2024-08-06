d837473cbbbc297e509601d346fad15991983892
Mary Louise Hermosa <jayzces@gmail.com>
2024-08-06 14:26:11 +0200

    Add success state for after form submission

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -6,6 +6,7 @@ const nextDay = ref(new Date(today))
 nextDay.value.setDate(nextDay.value.getDate() + 1)
 
 const checkIn = ref('')
+const successState = ref(false)
 
 const formatDate = (date: Date) => {
   return `${date.getFullYear()}-${padNumber(date.getMonth() + 1)}-${padNumber(date.getDate())}`
@@ -23,43 +24,61 @@ watch(checkIn, (newVal) => {
 </script>
 
 <template>
-  <form class="bg-russet/70 rounded-xl px-5 py-6 flex flex-wrap gap-4 w-[420px]">
-    <h5 class="text-cream w-full text-center">Contact us to book your room</h5>
-    <input
-      type="text"
-      name="name"
-      class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
-      placeholder="Name"
-    />
-    <input
-      type="text"
-      name="mobile"
-      class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
-      placeholder="Phone Number"
-    />
-    <input
-      type="email"
-      name="email"
-      class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
-      placeholder="Email"
-    />
-    <input
-      type="date"
-      name="check-in"
-      class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-[calc(50%_-_8px)]"
-      :min="formatDate(today)"
-      placeholder="Check In"
-      v-model="checkIn"
-    />
-    <input
-      type="date"
-      name="check-out"
-      class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-[calc(50%_-_8px)]"
-      :min="formatDate(nextDay)"
-      placeholder="Check Out"
-    />
-    <button type="submit" class="w-full rounded-md bg-olive text-white leading-[60px]">
-      Contact
-    </button>
+  <form
+    class="bg-russet/70 rounded-xl px-5 py-6 w-[420px] relative"
+    @submit.prevent="() => (successState = true)"
+  >
+    <div class="flex flex-wrap gap-4" :class="successState ? 'opacity-0' : 'opacity-100'">
+      <h5 class="text-cream w-full text-center">Contact us to book your room</h5>
+      <input
+        type="text"
+        name="name"
+        class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
+        placeholder="Name"
+        required
+      />
+      <input
+        type="text"
+        name="mobile"
+        class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
+        placeholder="Phone Number"
+        required
+      />
+      <input
+        type="email"
+        name="email"
+        class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-full"
+        placeholder="Email"
+        required
+      />
+      <input
+        type="date"
+        name="check-in"
+        class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-[calc(50%_-_8px)]"
+        :min="formatDate(today)"
+        placeholder="Check In"
+        v-model="checkIn"
+        required
+      />
+      <input
+        type="date"
+        name="check-out"
+        class="px-4 leading-[48px] bg-cream text-russet rounded-md block placeholder:text-clay w-[calc(50%_-_8px)]"
+        :min="formatDate(nextDay)"
+        placeholder="Check Out"
+        required
+      />
+      <button type="submit" class="w-full rounded-md bg-olive text-cream leading-[60px]">
+        Contact
+      </button>
+    </div>
+
+    <div
+      v-if="successState"
+      class="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center"
+    >
+      <h5 class="roboto-serif text-xl text-spring-sage">Thank you for your message!</h5>
+      <p class="mt-2 text-cream">We will contact you within the next 48 hours</p>
+    </div>
   </form>
 </template>
